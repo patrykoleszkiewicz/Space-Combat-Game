f37f2a182cd6c462e8ea412137bb1c2d9b3c695f
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-02-09 20:25:10 +0100

    Add basic operators to Vector2d

--- Include/Vector2d.hpp
+++ Include/Vector2d.hpp
@@ -11,5 +11,11 @@ public:
 	Vector2d(double X, double Y);
 	Vector2d(Vector2d &V);
 	
+	double length();
+	const Vector2d unit();
 	
+	const Vector2d operator+(Vector2d const &V);
+	const Vector2d operator-(Vector2d const &V);
+	const Vector2d operator*(double number);
+	const Vector2d operator/(double number);
 };
--- Source/Vector2d.cpp
+++ Source/Vector2d.cpp
@@ -1,5 +1,7 @@
 #include "Vector2d.hpp"
 
+#include <cmath>
+
 Vector2d::Vector2d()
 {
 	x = 0.0;
@@ -16,4 +18,50 @@ Vector2d::Vector2d(Vector2d &V)
 {
 	x = V.x;
 	y = V.y;
+}
+
+double Vector2d::length()
+{
+	return sqrt(x*x + y*y);
+}
+
+const Vector2d Vector2d::unit()
+{
+	double len = length();
+	Vector2d result;
+	result.x = x / len;
+	result.y = y / len;
+	return result;
+}
+
+const Vector2d Vector2d::operator+(Vector2d const &V)
+{
+	Vector2d result;
+	result.x = x + V.x;
+	result.y = y + V.y;
+	return result;
+}
+
+const Vector2d Vector2d::operator-(Vector2d const &V)
+{
+	Vector2d result;
+	result.x = x - V.x;
+	result.y = y - V.y;
+	return result;
+}
+
+const Vector2d Vector2d::operator*(double number)
+{
+	Vector2d result;
+	result.x = x * number;
+	result.y = y * number;
+	return result;
+}
+
+const Vector2d Vector2d::operator/(double number)
+{
+	Vector2d result;
+	result.x = x / number;
+	result.y = y / number;
+	return result;
 }
