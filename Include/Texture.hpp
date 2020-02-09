d1f109482a81ada26bd1dd01000345a25f807247
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-02-09 16:47:36 +0100

    Add getter and setter for Texture member _name

--- Include/Texture.hpp
+++ Include/Texture.hpp
@@ -5,6 +5,10 @@
 class Texture :public sf::Texture
 {
 public:
+	Texture(std::string name);
+	
+	void setName(std::string name);
+	
 	std::string getName();
 private:
 	std::string _name;
--- Source/Texture.cpp
+++ Source/Texture.cpp
@@ -1,5 +1,15 @@
 #include "Texture.hpp"
 
+Texture::Texture(std::string name)
+{
+	_name = name;
+}
+
+void Texture::setName(std::string name)
+{
+	_name = name;
+}
+
 std::string Texture::getName()
 {
 	return _name;
