62e6dd3e4049635561db62c648f03744286493e7
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-02-10 12:12:35 +0100

    Enhance Spaceship drawing system

--- Source/Spaceship.cpp
+++ Source/Spaceship.cpp
@@ -4,18 +4,18 @@
 
 void Spaceship::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-	Vector2d textureSize(_texture.getSize());
+	sf::Vector2f textureSize(_texture.getSize());
 	
-	Vector2d topLeft(-textureSize.x / 2.0, -textureSize.y / 2.0);
-	Vector2d topRight(textureSize.x / 2.0, -textureSize.y / 2.0);
-	Vector2d bottomLeft(-textureSize.x / 2.0, textureSize.y / 2.0);
-	Vector2d bottomRight(textureSize.x / 2.0, textureSize.y / 2.0);
+	sf::Vector2f topLeft(-textureSize.x / 2.0, -textureSize.y / 2.0);
+	sf::Vector2f topRight(textureSize.x / 2.0, -textureSize.y / 2.0);
+	sf::Vector2f bottomLeft(-textureSize.x / 2.0, textureSize.y / 2.0);
+	sf::Vector2f bottomRight(textureSize.x / 2.0, textureSize.y / 2.0);
 	
 	sf::VertexArray drawable(sf::TriangleStrip);
-	drawable.append(sf::Vertex(sf::Vector2f(0.0f,0.0f), sf::Vector2f(0.0f,0.0f)));
-	drawable.append(sf::Vertex(sf::Vector2f(141.0f,0.0f), sf::Vector2f(141.0f,0.0f)));
-	drawable.append(sf::Vertex(sf::Vector2f(0.0f,158.0f), sf::Vector2f(0.0f,158.0f)));
-	drawable.append(sf::Vertex(sf::Vector2f(141.0f,158.0f), sf::Vector2f(141.0f,158.0f)));
+	drawable.append(sf::Vertex(topLeft, sf::Vector2f(0.0f, 0.0f)));
+	drawable.append(sf::Vertex(topRight, sf::Vector2f(textureSize.x, 0.0f)));
+	drawable.append(sf::Vertex(bottomLeft, sf::Vector2f(0.0f, textureSize.y)));
+	drawable.append(sf::Vertex(bottomRight, sf::Vector2f(textureSize.x, textureSize.y)));
 	
 	states.texture = &_texture;
 	target.draw(drawable, states);
