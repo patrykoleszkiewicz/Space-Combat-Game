33b1bf4e7e31536220abb5d7c82d734a59f3125c
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-02-11 16:13:31 +0100

    Implement updateEngines method of Controlable

--- Source/Controlable.cpp
+++ Source/Controlable.cpp
@@ -21,8 +21,18 @@ void Controlable::avionics()
 
 void Controlable::updateEngines()
 {
+	Vector2d thrustTotal(0.0,0.0);
+	double momentumTotal = 0.0;
+	
 	for(auto& thruster : _thrusters)
 	{
+		Vector2d thrust = Vector2d(0.0,-1.0).rotate(thruster.angle) * thruster.thrust * thruster.throttle;
+		thrustTotal += thrust;
 		
+		double momentum = thruster.position.x * thrust.y - thruster.position.y * thrust.x;
+		momentumTotal += momentum;
 	}
+	
+	_velocity += thrustTotal / _mass;
+	_angleVelocity += momentumTotal / _mass;
 }
