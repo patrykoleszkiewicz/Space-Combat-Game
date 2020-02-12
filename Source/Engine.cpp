a6a30a044a38a1c612cc84c9bc7aba8453cfe7d6
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-02-12 12:43:39 +0100

    Remove test code

--- Source/Engine.cpp
+++ Source/Engine.cpp
@@ -6,30 +6,6 @@ int Engine::start()
 {
 	_spaceships.clear();
 	
-	//TEST CODE
-	Spaceship ship;
-	ship.setTexture(_textures.at(0));
-	ship.setPilot((Pilot*)new PilotPlayer());
-	
-	ship.setPosition(640.0,360.0);
-	
-	Thruster th;
-	th.thrust = 1.0;
-	th.throttle = 0.0;
-	th.angle = 0.0;
-	th.position = Vector2d(0.0,0.0);
-	
-	th.forward = true;
-	th.backward = false;
-	th.left = false;
-	th.right = false;
-	th.rotateLeft = false;
-	th.rotateRight = false;
-	
-	ship.addThruster(th);
-	
-	_spaceships.push_back(ship);
-	
 	return 0;
 }
 
