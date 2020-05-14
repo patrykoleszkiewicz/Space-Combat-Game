dde9637bb4edcf84d8d8028a2192a635635e7829
patrykoleszkiewicz <patryk.oleszkiewicz12@gmail.com>
2020-05-14 15:24:34 +0200

    Modify default matrix

--- Source/Matrix4x4.cpp
+++ Source/Matrix4x4.cpp
@@ -20,7 +20,7 @@ Matrix4x4::Matrix4x4()
     mat[3][0] = 0.0;
     mat[3][1] = 0.0;
     mat[3][2] = 0.0;
-    mat[3][3] = 0.0;
+    mat[3][3] = 1.0;
 }
 
 Vector3d Matrix4x4::multiplyByVector(const Vector3d &V) const
